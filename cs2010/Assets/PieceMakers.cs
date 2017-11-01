83391f1f5c01e1f539e1d5965f7e97256cc03dde
Matt <Matthewbarlow98@gmail.com>
2017-11-01 23:36:53 +0000

    added comments

--- cs2010/Assets/PieceMakers.cs
+++ cs2010/Assets/PieceMakers.cs
@@ -3,44 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PieceMakers : MonoBehaviour {
-
+	//records what a white and black piece look like
 	public GameObject blackPiecePrefab;
 	public GameObject whitePiecePrefab;
 	
+	//records the location of this placeholder
 	public int x;
 	public int y;
+	//global variable of turns, used to determine if black or white turn
 	private static int turns = 0;
-	private static bool[,] boardRecord = new bool[16,16];
-	// Use this for initialization
+	//array of gameObjects, not sure if it stores the actual objects or just clones
+	private static GameObject[,] boardRecord = new GameObject[16,16];
+	
 	public void Initialize(int x, int y) 
 	{
+		//sets this x and y when it is called inside of checkerboard
 		this.x = x;
 		this.y = y;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 	void OnMouseDown()
 	{
-		
-//		for(int y = 0; y < 16; y++)
-//        {
-//            for(int x = 0; x < 16; x++)
-//            {
-//				Debug.Log(x +","+ y +" : "+boardRecord[x,y]);
-//            }
-//        }
-		if(boardRecord[x,y]==false)
+		//if the current slot is empty
+		if(boardRecord[x,y]==null)
 		{
-			boardRecord[x,y] = true;
+			//sets obj to black or white depending on turn
+			var obj = (turns % 2 == 0)?blackPiecePrefab:whitePiecePrefab;
+			//adds to array
+			boardRecord[x,y] = obj;
+			//increments turn
 			turns++;
 			var pos = this.transform.position;
+			//pushes the position of the new piece up a bit just to make it fit better
 			pos.y = 0.15f;
+			//rotates it to fit nicer
 			var rot = Quaternion.Euler(-90,0,0);
-
-			var newPiece = Instantiate((turns % 2 == 0)?blackPiecePrefab:whitePiecePrefab, pos, rot);
+			//places it in the scene
+			var newPiece = Instantiate(obj, pos, rot);
 		}else
 		{
 			Debug.Log("Already a piece there!!!");
