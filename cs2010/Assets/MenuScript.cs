af99cfe853123bd3ca00ea053b64342077f3bcc7
Li He <helk@aston.ac.uk>
2017-12-03 20:06:14 +0000

    litty

---
+++ cs2010/Assets/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void PlayGame ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
