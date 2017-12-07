73a45ac10093443ea8c17c0f22d10835f271a30d
Li He <helk@aston.ac.uk>
2017-12-07 17:15:05 +0000

    pleasse

--- cs2010/Assets/Scripts/HUDScript.cs
+++ cs2010/Assets/Scripts/HUDScript.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+
 
 public class HUDScript : MonoBehaviour {
 
 	public Text blackCountText;
 	public Text whiteCountText;
+	public Text blackCaptureText;
+	public Text whiteCaptureText;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +25,8 @@ public class HUDScript : MonoBehaviour {
 	void SetCountText() {
 		blackCountText.text = "Black Stones: " + PieceMakers.bCount;
 		whiteCountText.text = "White Stones: " + PieceMakers.wCount;
+		blackCaptureText.text = "Capture: " + PieceMakers.wCount;
+		whiteCaptureText.text = "Capture: " + PieceMakers.bCount;
 	}
 
 
