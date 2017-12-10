ba73541496aa6a4b6b2e25b05bf55fa84e44b084
Li He <helk@aston.ac.uk>
2017-12-10 22:09:05 +0000

    got textmesh pro hud working, will finish tomorrow

--- cs2010/Assets/Scripts/HUDScript.cs
+++ cs2010/Assets/Scripts/HUDScript.cs
@@ -14,10 +14,10 @@ public class HUDScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		blackCountText = GetComponent<TextMeshProUGUI>() ?? gameObject.AddComponent<TextMeshProUGUI>();
-		whiteCountText = GetComponent<TextMeshProUGUI>() ?? gameObject.AddComponent<TextMeshProUGUI>();
-		blackCaptureText = GetComponent<TextMeshProUGUI>() ?? gameObject.AddComponent<TextMeshProUGUI>();
-		whiteCaptureText = GetComponent<TextMeshProUGUI> () ?? gameObject.AddComponent<TextMeshProUGUI>();
+		blackCountText = GetComponent<TextMeshProUGUI>();
+		whiteCountText = GetComponent<TextMeshProUGUI>();
+		blackCaptureText = GetComponent<TextMeshProUGUI>();
+		whiteCaptureText = GetComponent<TextMeshProUGUI>();
 
 		SetCountText ();
 	}
@@ -29,15 +29,10 @@ public class HUDScript : MonoBehaviour {
 
 
 	void SetCountText() {
-		blackCountText.SetText ("Black Stones: " + PieceMakers.bCount);
-		whiteCountText.SetText ("White Stones: " + PieceMakers.wCount);
-		blackCaptureText.SetText ("Capture: " + PieceMakers.bCapture);
-		whiteCaptureText.SetText ("Capture: " + PieceMakers.wCapture);
-
 		blackCountText.text = "Black Stones: " + PieceMakers.bCount;
-		whiteCountText.text = "White Stones: " + PieceMakers.wCount;
-		blackCaptureText.SetText ("Capture: " + PieceMakers.bCapture);
-		whiteCaptureText.SetText ("Capture: " + PieceMakers.wCapture);
+		whiteCountText.text = "White Stones: " +  PieceMakers.wCount;
+		blackCaptureText.text = "Capture:  " +  PieceMakers.bCapture;
+		whiteCaptureText.text = "Capture:  " +  PieceMakers.wCapture;
 	}
 
 
