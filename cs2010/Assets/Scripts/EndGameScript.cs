301e400682f3699ee8ab1fd105621f7225f31cfa
Li He <helk@aston.ac.uk>
2017-12-12 19:53:37 +0000

    winner text, implemented menu button but needs script

--- cs2010/Assets/Scripts/EndGameScript.cs
+++ cs2010/Assets/Scripts/EndGameScript.cs
@@ -1,4 +1,4 @@
-﻿﻿using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,10 +11,13 @@ public class EndGameScript : MonoBehaviour {
 	public TextMeshProUGUI blackScore;
 	public TextMeshProUGUI whiteScore;
 
+	public TextMeshProUGUI winnerText;
+
 	// Use this for initialization
 	void Start () {
 		blackScore = GetComponent<TextMeshProUGUI> ();
 		whiteScore = GetComponent<TextMeshProUGUI> ();
+		winnerText = GetComponent<TextMeshProUGUI> ();
 	}
 
 	// Update is called once per frame
@@ -24,6 +27,12 @@ public class EndGameScript : MonoBehaviour {
 			whiteScore.text = score.ToString ();
 			score = PieceMakers.bCapture;
 			blackScore.text = score.ToString ();
+			if(PieceMakers.wCapture > PieceMakers.bCapture){
+				winnerText.text = "White Win";
+			}
+			else{
+				winnerText.text = "Black Win";
+			}
 			endPopUp.SetActive (true);
 		} else {
 			endPopUp.SetActive (false);
