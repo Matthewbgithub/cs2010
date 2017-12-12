2bae6d266fe08f1283a9a2c0d63696f4e64d7a72
Li He <helk@aston.ac.uk>
2017-12-12 20:59:51 +0000

    Added turn text, winner text and menu button(- logic ) on end game

---
+++ cs2010/Assets/Scripts/HUDScripts/TurnScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TurnScript : MonoBehaviour {
+
+	private TextMeshProUGUI turnText;
+	// Use this for initialization
+	void Start () {
+		turnText = GetComponent<TextMeshProUGUI> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Finds and display which players turn it is
+		if(PieceMakers.turns%2 == 0){
+			turnText.text = "Black Pebble";
+		}
+		else{
+			turnText.text = "White Pebble";
+		}
+
+	}
+}
