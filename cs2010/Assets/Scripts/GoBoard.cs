f4f359f844b6d9a7a19c37fbebaa1d6404af279d
Matt <Matthewbarlow98@gmail.com>
2018-01-30 00:57:36 +0000

    prepping for refactor

--- cs2010/Assets/Scripts/GoBoard.cs
+++ cs2010/Assets/Scripts/GoBoard.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class GoBoard : MonoBehaviour {
 
-	public Piece[,] Pieces = new Piece[16,16];
+	public Piece[,] Pieces;
     public GameObject whitePiecePrefab;
     public GameObject blackPiecePrefab;
 	public GameObject piecePlaceHolder;
@@ -17,6 +17,9 @@ public class GoBoard : MonoBehaviour {
 	
     private void Start()
     {
+		int boardXSize = 16;
+		int boardYSize = 16;
+		Pieces = new Piece[boardXSize,boardYSize];
         GenerateBoard();
     }
 
@@ -36,9 +39,75 @@ public class GoBoard : MonoBehaviour {
 			}
 		}
     }
-	private void MovePlaceholder( GameObject g, int x, int y)
-	{
-		g.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
-	}
-	
+    private void MovePlaceholder(GameObject g, int x, int y)
+    {
+        g.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
+    }
+    private void PlacePiece()
+    {
+        //places piece on the board
+    }
+    private void CheckForCaptures(int checkX, int checkY)
+    {
+        //start at x and y and then scan about to find any big captures
+
+        //this method will need some functionality from check method
+    }
+    //replace check method with two
+    private Boolean IsColourDifferent(int x, int y, PieceColour p)
+    {
+        //checks whether the piece is different colour or same
+    }
+    private Boolean IsPlaceEmpty(int x, int y)
+    {
+        //return true if empty
+    }
+    //-------
+    //whole board checker
+    private void SetBoardChecked(int x, int y)
+    {
+        //addtocheckedlist
+    }
+    private Boolean IsBoardChecked(int x, int y)
+    {
+        //is already checked
+    }
+    private void ResetBoardChecked()
+    {
+        //resets list of currently checked items
+    }
+    //-------
+    //group checker
+    private void SetGroupChecked(int x, int y)
+    {
+        //addtocheckedlist
+    }
+    private Boolean IsGroupChecked(int x, int y)
+    {
+        //is already checked
+    }
+    private void ResetGroupChecked()
+    {
+        //resets list of currently checked items
+    }
+    private void RemoveCaptured()
+    {
+        //removes everything in current capture list
+    }
+    private void CheckSurrounding(int x, int y)
+    {
+        //think you should merge this with search neighbours perhaps
+    }
+    private void SetCaptured(int x, int y)
+    {
+        //add to capute group
+    }
+    private bool IsOffBoard(int x, int y)
+    {
+        
+    }
+    private void Remove(int x, int y)
+    {
+
+    }
 }
