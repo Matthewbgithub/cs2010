f5c8991ff2761c13053ff92b448136bf1b0d35ad
liknhe <liknhe@gmail.com>
2018-02-13 18:51:10 +0000

    o..k.

--- cs2010/Assets/Tests/CaptureTests.cs
+++ cs2010/Assets/Tests/CaptureTests.cs
@@ -3,15 +3,18 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class CaptureTests {
-    
+public class CaptureTests
+{
+    [Test]
+    public void Testing_For_False()
+    {
+        bool haha = true;
+        Assert.AreEqual(true, haha);
+    }
+
     [Test]
     public void Test_Stone_Placement()
     {
-        GoBoard board = new GoBoard();
-        //PieceMakers pMaker = null;
-
-        board.Start();
         //board.TakeTurn(2,2);//black
         Assert.AreEqual(false, board.TakeTurn(2, 2));
         //board.TakeTurn(2,3);//white
@@ -20,12 +23,5 @@ public class CaptureTests {
         //board.TakeTurn(9, 9);
 
         //Assert.AreEqual();
-    }
-
-    [Test]
-    public void Test_Stone_Capture(){
-        //board.TakeTurn(2,2);//black
-        //board.TakeTurn(2,3);//white
-        //board.TakeTurn(1,3);//black
     }
 }
