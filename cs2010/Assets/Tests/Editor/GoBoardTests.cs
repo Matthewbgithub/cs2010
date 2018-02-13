44794d4656611c07eefcf4f7b4d949441961ab2a
Tom Bagley <tom@Toms-MBP.lan>
2018-02-13 15:41:23 +0000

    Initial tests written

---
+++ cs2010/Assets/Tests/Editor/GoBoardTests.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class GoBoardTests {
+
+	private GoBoard board;
+
+	[SetUp]
+	public void Setup() {
+		// Incorrect way to invoke the GoBoard script. Need to create Board object then attach script to it.
+		board = new GoBoard();
+	}
+
+	[Test]
+	public void TestBlackCounterInit() {
+		int blackCount = board.GetBlackCount();
+
+		// Black counter should initialise to 0
+		Assert.AreEqual(blackCount, 0);
+	}
+
+	[Test]
+	public void TestWhiteCounterInit() {
+		int whiteCount = board.GetWhiteCount();
+
+		// Black counter should initialise to 0
+		Assert.AreEqual(whiteCount, 0);
+	}
+		
+}
