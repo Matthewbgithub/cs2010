5903a0572cc18a0d3a2a68f7d99c5216cdc89f5c
liknhe <liknhe@gmail.com>
2018-03-18 11:59:47 +0000

    quit button added

---
+++ cs2010/Assets/QuitScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class QuitScript : MonoBehaviour
+{
+
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+
+}
