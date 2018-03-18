e11783f2c80dbf35e0886e85303d4044f786d612
Tim Bettison <bettisont@gmail.com>
2018-03-18 18:44:10 +0000

    trying to add sound fx

---
+++ cs2010/Assets/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake () {
+		foreach (Sound s in sounds) {
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ cs2010/Assets/buttonOnClick.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+[RequireComponent(typeof(Button))]
+
+public class buttonOnClick : MonoBehaviour
+{
+
+	public GameObject NextWindow;
+
+	public GameObject PreviousWindow;
+
+	public AudioClip sound;
+
+	private Button button { get { return GetComponent<Button> (); } }
+
+	private AudioSource source { get { return GetComponent<AudioSource> (); } }
+
+	void Start(){
+		gameObject.AddComponent<AudioSource> ();
+		source.clip = sound;
+		source.playOnAwake = false;
+		PreviousWindow.SetActive (false);
+		button.onClick.AddListener (() => playSound ());
+		//button.onClick.AddListener (() => clickMethod ());
+
+	}
+
+	void playSound(){
+		source.PlayOneShot (sound);
+		clickMethod();
+	}
+
+
+	void clickMethod(){
+		NextWindow.SetActive(true);
+
+	}
+
+}
+
+//using UnityEngine;
+//using UnityEngine.UI;
+//[RequireComponent(typeof(Button))]
+//
+//public class buttonOnClick : MonoBehaviour
+//{
+//
+//	public GameObject StartWindow;
+//
+//	public GameObject BoardWindow;
+//
+//	public AudioClip sound;
+//
+//	private Button button { get { return GetComponent<Button> (); } }
+//
+//	private AudioSource source { get { return GetComponent<AudioSource> (); } }
+//
+//	void Start(){
+//		gameObject.AddComponent<AudioSource> ();
+//		source.clip = sound;
+//		source.playOnAwake = false;
+//		button.onClick.AddListener (() => playSound ();
+//			}
+//
+//			void clickMethod(){
+//				playSound ();
+//				//StartWindow.SetActive(false);
+//				//BoardWindow.SetActive(true);
+//			}
+//
+//			void playSound(){
+//				source.PlayOneShot (sound);
+//			}
+//
+//
+//			}
